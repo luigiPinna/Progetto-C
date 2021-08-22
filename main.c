10ffe524ea853f4ccde8046cdf9c46c5ec14a1d0
Luigi Pinna <89350098+luigiPinna@users.noreply.github.com>
2021-08-22 18:05:40 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+/**************************************************************************************************
+ * Primo Progetto individuale TNV Programmazione C - Modulo II                                    *
+ *                                                                                                *
+ * Author: Luigi Pinna                                                                            *
+ * Date: 07/06/2021                                                                               *
+ **************************************************************************************************/
+#include "input.h"
+
+int main() {
+    avviaProgrammaCriminali();      //Programma di gestione criminali
+
+    return 0;
+}
+
+
