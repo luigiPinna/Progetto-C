e22f247a21d8f62559b1d424d5d2ba2f2bb15253
Luigi Pinna <89350098+luigiPinna@users.noreply.github.com>
2021-08-22 18:18:31 +0200

    Add files via upload

---
+++ controlliInput.h
@@ -0,0 +1,15 @@
+#ifndef PROGETTOC_CONTROLLIINPUT_H
+#define PROGETTOC_CONTROLLIINPUT_H
+#define ENUM_MAX 3
+
+/*Procedure di controllo input*/
+void controlloAltezza(RecordSoggetto *input);
+void controlloPeso(RecordSoggetto *input);
+void controlloLatitudine(RecordSoggetto *input);
+void controlloLongitudine(RecordSoggetto *input);
+void controlloBoolBarba(RecordSoggetto *input);
+void controlloBoolCicatrici(RecordSoggetto *input);
+void controlloStato(RecordSoggetto *input);
+void controlloTipoCapelli(RecordSoggetto *input);
+
+#endif //PROGETTOC_CONTROLLIINPUT_H
